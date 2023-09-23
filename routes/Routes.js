6726dff935b0d30322955ebddfe26ad73d653eb3
Ogaraga <raymond.ogaraga@gmail.com>
2023-09-23 01:45:14 +0100

    review

---
+++ routes/Routes.js
@@ -0,0 +1,19 @@
+//importing packages
+const router = require('express').Router();
+const {setPost,getPost} = require('../controllers/register');
+const {login_get, login_post} = require('../controllers/login');
+const getPort = require('../controllers/portfolio');
+
+//register routes
+router.post('/', setPost);
+router.get('/', getPost);
+
+//login routes
+router.post('/login', login_post);
+router.get('/login', login_get); 
+
+
+//portfolio route and jwt token validation
+router.get('/portfolio', getPort);
+
+module.exports = router;
